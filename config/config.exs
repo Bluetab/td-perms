51abb197f4d3732a0ef5ea972172b31de7ab153c
Tom Crossland <tom.crossland@bluetab.net>
2018-07-02 19:34:03 +0200

    feat: expiry, refactoring

--- config/config.exs
+++ config/config.exs
@@ -20,6 +20,30 @@ use Mix.Config
 #
 #     config :logger, level: :info
 #
+config :td_perms, permissions: [
+  :is_admin,
+  :create_acl_entry,
+  :update_acl_entry,
+  :delete_acl_entry,
+  :create_domain,
+  :update_domain,
+  :delete_domain,
+  :view_domain,
+  :create_business_concept,
+  :update_business_concept,
+  :send_business_concept_for_approval,
+  :delete_business_concept,
+  :publish_business_concept,
+  :reject_business_concept,
+  :deprecate_business_concept,
+  :manage_business_concept_alias,
+  :view_draft_business_concepts,
+  :view_approval_pending_business_concepts,
+  :view_published_business_concepts,
+  :view_versioned_business_concepts,
+  :view_rejected_business_concepts,
+  :view_deprecated_business_concepts
+]
 
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
--- lib/td_perms/permissions.ex
+++ lib/td_perms/permissions.ex
@@ -1,52 +1,42 @@
 defmodule TdPerms.Permissions do
-  @permissions [
-    :is_admin,
-    :create_acl_entry,
-    :update_acl_entry,
-    :delete_acl_entry,
-    :create_domain,
-    :update_domain,
-    :delete_domain,
-    :view_domain,
-    :create_business_concept,
-    :update_business_concept,
-    :send_business_concept_for_approval,
-    :delete_business_concept,
-    :publish_business_concept,
-    :reject_business_concept,
-    :deprecate_business_concept,
-    :manage_business_concept_alias,
-    :view_draft_business_concepts,
-    :view_approval_pending_business_concepts,
-    :view_published_business_concepts,
-    :view_versioned_business_concepts,
-    :view_rejected_business_concepts,
-    :view_deprecated_business_concept
-  ] |> Enum.with_index |> Map.new
+  @permissions Application.get_env(:td_perms, :permissions)
+    |> Enum.with_index |> Map.new
 
   def permissions, do: @permissions |> Map.keys
 
+  def has_permission(session_id, permission, resource_type, resource_id) when is_bitstring(permission) do
+    has_permission(session_id, String.to_atom(permission), resource_type, resource_id)
+  end
+
   def has_permission(session_id, permission, resource_type, resource_id) do
-    key = ["jti", session_id, resource_type, resource_id]
-      |> Enum.join(":")
+    key = get_key(session_id, resource_type, resource_id)
     cmds = Map.get(@permissions, permission)
       |> get_bit_cmd
-    Redix.command(:redix, ["BITFIELD"|[key|cmds]])
+    {:ok, bits} = Redix.command(:redix, ["BITFIELD"|[key|cmds]])
+    bits |> Enum.any?(&(&1 > 0))
   end
 
-  def cache_session_permissions(session_id, acl_entries) do
+  def cache_session_permissions!(session_id, expire_at, acl_entries) do
     pipeline = acl_entries
-      |> Enum.map(&(entry_to_command(session_id, &1)))
-    Redix.pipeline(:redix, pipeline)
+      |> Enum.flat_map(&(entry_to_commands(session_id, expire_at, &1)))
+    Redix.pipeline!(:redix, pipeline)
   end
 
-  defp entry_to_command(jti, %{resource_type: resource_type, resource_id: resource_id, permissions: perms}) do
-    key = ["jti", jti, resource_type, resource_id]
-      |> Enum.join(":")
+  defp entry_to_commands(session_id, expire_at, %{resource_type: resource_type, resource_id: resource_id, permissions: perms}) do
+    key = get_key(session_id, resource_type, resource_id)
     cmds = perms
+      |> Enum.map(& String.to_atom/1)
       |> Enum.map(&(Map.get(@permissions, &1)))
       |> Enum.flat_map(&set_bit_cmd/1)
-    ["BITFIELD"|[key|cmds]]
+    [
+      ["BITFIELD"|[key|cmds]],
+      ["EXPIREAT", key, expire_at]
+    ]
+  end
+
+  defp get_key(session_id, resource_type, resource_id) do
+    ["session", session_id, resource_type, resource_id]
+      |> Enum.join(":")
   end
 
   defp set_bit_cmd(offset), do: ["SET", "u1", offset, 1]
