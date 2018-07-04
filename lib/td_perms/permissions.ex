887dde313340f19c05ad381e345dd09807867b64
Tom Crossland <tom.crossland@bluetab.net>
2018-07-04 09:46:00 +0200

    feat: read acls by resource type

--- lib/td_perms/permissions.ex
+++ lib/td_perms/permissions.ex
@@ -2,7 +2,15 @@ defmodule TdPerms.Permissions do
   @permissions Application.get_env(:td_perms, :permissions)
     |> Enum.with_index |> Map.new
 
+  @permissions_by_offset Application.get_env(:td_perms, :permissions)
+    |> Enum.with_index
+    |> Enum.map(fn {a, b} -> {b, a} end)
+    |> Enum.sort
+    |> Enum.map(fn {_, b} -> b end)
+
   def permissions, do: @permissions |> Map.keys
+
+  def perms, do: @permissions
 
   def has_permission?(session_id, permission, resource_type, resource_id) when is_bitstring(permission) do
     has_permission?(session_id, String.to_atom(permission), resource_type, resource_id)
@@ -14,6 +22,26 @@ defmodule TdPerms.Permissions do
       |> get_bit_cmd
     {:ok, bits} = Redix.command(:redix, ["BITFIELD"|[key|cmds]])
     bits |> Enum.any?(&(&1 > 0))
+  end
+
+  def get_acls_by_resource_type(session_id, resource_type) do
+    pattern = get_key_pattern(session_id, resource_type)
+    Redix.command!(:redix, ["KEYS", pattern])
+      |> Enum.map(&read_acl_entry(&1))
+  end
+
+  defp read_acl_entry(key) do
+    permissions = Redix.command!(:redix, ["GET", key])
+     |> bitstring_to_list
+     |> Enum.zip(@permissions_by_offset)
+     |> Enum.filter(fn {bit, _} -> bit == 1 end)
+     |> Enum.map(fn {_, perm} -> perm end)
+    [_, _, resource_type, resource_id] = key |> String.split(":")
+    %{resource_type: resource_type, resource_id: String.to_integer(resource_id), permissions: permissions}
+  end
+  defp bitstring_to_list(<<>>), do: []
+  defp bitstring_to_list(<<x::size(1), rest::bitstring>>) do
+    [x|bitstring_to_list(rest)]
   end
 
   def cache_session_permissions!(session_id, expire_at, acl_entries) do
@@ -34,6 +62,10 @@ defmodule TdPerms.Permissions do
     ]
   end
 
+  defp get_key_pattern(session_id, resource_type) do
+    ["session", session_id, resource_type, "*"]
+      |> Enum.join(":")
+  end
   defp get_key(session_id, resource_type, resource_id) do
     ["session", session_id, resource_type, resource_id]
       |> Enum.join(":")
