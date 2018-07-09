bdfff0bebcaca25ebb504f0b9fcb20e3720bcee6
Tom Crossland <tom.crossland@bluetab.net>
2018-07-09 12:16:03 +0200

    chore: add credo dependency

--- mix.exs
+++ mix.exs
@@ -22,9 +22,8 @@ defmodule TdPerms.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:redix, ">= 0.0.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:redix, ">= 0.0.0"},
+      {:credo, "~> 0.9.3", only: [:dev, :test], runtime: false}
     ]
   end
 end
