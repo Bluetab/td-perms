8990b9967252082a1f2ffafe4d3fdef8240bb494
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-07-26 14:33:26 +0200

    feat: Include function to query all domains

--- lib/td_perms/taxonomy_cache.ex
+++ lib/td_perms/taxonomy_cache.ex
@@ -21,8 +21,7 @@ defmodule TdPerms.TaxonomyCache do
 
   def get_name(domain_id) do
     key = create_key(domain_id)
-    {:ok, name} = Redix.command(:redix, ["HGET", key, "name"])
-    name
+    get_domain_name(key)
   end
 
   def put_domain(%{id: domain_id, parent_ids: parent_ids, name: name}) do
@@ -34,6 +33,23 @@ defmodule TdPerms.TaxonomyCache do
   def delete_domain(domain_id) do
     key = create_key(domain_id)
     Redix.command(:redix, ["DEL", key])
+  end
+
+  def get_all_domains do
+    key = "domain:*"
+    {:ok, keys} = Redix.command(:redix, ["KEYS", key])
+    Enum.map(keys, &get_domain(&1))
+  end
+
+  defp get_domain(key) do
+    %{}
+      |> Map.put(:name, get_domain_name(key))
+      |> Map.put(:domain_id, key |> String.split(":") |> List.last())
+  end
+
+  defp get_domain_name(key) do
+    {:ok, name} = Redix.command(:redix, ["HGET", key, "name"])
+    name
   end
 
   defp create_key(domain_id) do
--- test/td_perms/taxonomy_cache_test.exs
+++ test/td_perms/taxonomy_cache_test.exs
@@ -11,7 +11,7 @@ defmodule TdPerms.TaxonomyCacheTest do
   test "get_parent_ids with self returns parent ids including domain_id" do
     domain = domain_fixture()
     TaxonomyCache.put_domain(domain)
-    assert TaxonomyCache.get_parent_ids(domain.id) == [domain.id|domain.parent_ids]
+    assert TaxonomyCache.get_parent_ids(domain.id) == [domain.id | domain.parent_ids]
   end
 
   test "get_parent_ids without self returns parent ids excluding domain_id" do
@@ -39,7 +39,28 @@ defmodule TdPerms.TaxonomyCacheTest do
     assert {:ok, 0} = Redix.command(:redix, ["EXISTS", "domain:#{domain.id}"])
   end
 
+  test "get_all_domains returns all the domains in the system" do
+    list_domain_fixture()
+    |> Enum.map(&TaxonomyCache.put_domain(&1))
+
+    result_list = TaxonomyCache.get_all_domains()
+
+    list_domain_fixture()
+    |> Enum.all?(fn (x) ->
+      Enum.any?(result_list, &(&1.name == x.name))
+    end)
+    |> assert
+  end
+
   defp domain_fixture do
     %{id: 1, parent_ids: [2, 3, 4], name: "foo"}
+  end
+
+  defp list_domain_fixture do
+    [
+      %{id: 1, parent_ids: [2, 3, 4], name: "foo_1"},
+      %{id: 2, parent_ids: [2, 3, 4], name: "foo_2"},
+      %{id: 3, parent_ids: [2, 3, 4], name: "foo_3"}
+    ]
   end
 end
