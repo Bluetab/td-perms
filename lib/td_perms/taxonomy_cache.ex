48ca850c787aaa4ffacbbe8707442571315d684e
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-07-27 11:06:09 +0200

    chore: Modify get_domain function to retrieve domain_id as integer

--- lib/td_perms/taxonomy_cache.ex
+++ lib/td_perms/taxonomy_cache.ex
@@ -4,19 +4,27 @@ defmodule TdPerms.TaxonomyCache do
   """
 
   def get_parent_ids(domain_id, with_self \\ true)
+
   def get_parent_ids(domain_id, false) do
     key = create_key(domain_id)
     {:ok, parent_ids} = Redix.command(:redix, ["HGET", key, "parent_ids"])
+
     case parent_ids do
-      nil -> []
-      "" -> []
-      ids -> ids
+      nil ->
+        []
+
+      "" ->
+        []
+
+      ids ->
+        ids
         |> String.split(",")
         |> Enum.map(&String.to_integer/1)
     end
   end
+
   def get_parent_ids(domain_id, true) do
-    [domain_id|get_parent_ids(domain_id, false)]
+    [domain_id | get_parent_ids(domain_id, false)]
   end
 
   def get_name(domain_id) do
@@ -51,14 +59,14 @@ defmodule TdPerms.TaxonomyCache do
   end
 
   defp retrieve_list_from_enumerator(cursor, key) do
-    {:ok, [head|tail]} = scan_command(cursor, key)
+    {:ok, [head | tail]} = scan_command(cursor, key)
     list_domains = get_domains_from_list_keys(tail |> List.flatten())
     {String.to_integer(head), list_domains}
   end
 
   defp get_domains_from_list_keys(list_keys) do
     list_keys
-     |> Enum.map(&get_domain(&1))
+    |> Enum.map(&get_domain(&1))
   end
 
   defp scan_command(cursor, key) do
@@ -67,8 +75,8 @@ defmodule TdPerms.TaxonomyCache do
 
   defp get_domain(key) do
     %{}
-      |> Map.put(:name, get_domain_name(key))
-      |> Map.put(:domain_id, key |> String.split(":") |> List.last())
+    |> Map.put(:name, get_domain_name(key))
+    |> Map.put(:domain_id, key |> String.split(":") |> List.last() |> String.to_integer())
   end
 
   defp get_domain_name(key) do
