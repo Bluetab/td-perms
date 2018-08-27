ee335b981f7c1821d005c8be6bd8900b3c6132fd
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-08-27 11:49:20 +0200

    chore: Bump library version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TdPerms.MixProject do
   def project do
     [
       app: :td_perms,
-      version: "0.3.4",
+      version: "0.3.5",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
