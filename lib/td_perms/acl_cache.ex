715952192a1c5ffb843c699e15f7e63ccf9095aa
netto <euclydes.young@gmail.com>
2018-10-24 12:39:43 +0200

    refactor: changes acl cache to store basic values instead of json structure

--- lib/td_perms/acl_cache.ex
+++ lib/td_perms/acl_cache.ex
@@ -2,33 +2,52 @@ defmodule TdPerms.AclCache do
   @moduledoc """
     Shared cache for Access Control List.
   """
-  def get_user_roles(resource_type, resource_id) do
-    key = create_key(resource_type, resource_id)
-    {:ok, user_roles} = Redix.command(:redix, ["HGET", key, "user_roles"])
-    Poison.decode!(user_roles)
+
+  def create_acl_roles_key(resource_type, resource_id) do
+    "acl_roles:#{resource_type}:#{resource_id}"
   end
 
-  def put_user_roles(%{
-        resource_type: resource_type,
-        resource_id: resource_id,
-        user_roles: user_roles
-      }) do
-    key = create_key(resource_type, resource_id)
-
-    Redix.command(:redix, [
-      "HMSET",
-      key,
-      "user_roles",
-      Poison.encode!(user_roles)
-    ])
+  def get_acl_roles(resource_type, resource_id) do
+    key = create_acl_roles_key(resource_type, resource_id)
+    {:ok, roles} = Redix.command(:redix, ["SMEMBERS", key])
+    roles
   end
 
-  def delete_user_roles(resource_type, resource_id) do
-    key = create_key(resource_type, resource_id)
+  def set_acl_roles(resource_type, resource_id, roles) when is_list(roles) do
+    key = create_acl_roles_key(resource_type, resource_id)
+    Redix.command(:redix, ["DEL", key])
+    Redix.command(:redix, ["SADD", key] ++ roles)
+  end
+  def set_acl_roles(resource_type, resource_id, roles = %MapSet{}) do
+    set_acl_roles(resource_type, resource_id, MapSet.to_list(roles))
+  end
+
+  def delete_acl_roles(resource_type, resource_id) do
+    key = create_acl_roles_key(resource_type, resource_id)
     Redix.command(:redix, ["DEL", key])
   end
 
-  def create_key(resource_type, resource_id) do
-    "acl_user_roles:#{resource_type}:#{resource_id}"
+  def create_acl_role_users_key(resource_type, resource_id, role) do
+    "acl_role_users:#{resource_type}:#{resource_id}:#{role}"
+  end
+
+  def get_acl_role_users(resource_type, resource_id, role) do
+    key = create_acl_role_users_key(resource_type, resource_id, role)
+    {:ok, role_users} = Redix.command(:redix, ["SMEMBERS", key])
+    role_users
+  end
+
+  def set_acl_role_users(resource_type, resource_id, role, users) when is_list(users) do
+    key = create_acl_role_users_key(resource_type, resource_id, role)
+    Redix.command(:redix, ["DEL", key])
+    Redix.command(:redix, ["SADD", key] ++ users)
+  end
+  def set_acl_role_users(resource_type, resource_id, role, users = %MapSet{}) do
+    set_acl_role_users(resource_type, resource_id, role, MapSet.to_list(users))
+  end
+
+  def delete_acl_role_users(resource_type, resource_id, role) do
+    key = create_acl_role_users_key(resource_type, resource_id, role)
+    Redix.command(:redix, ["DEL", key])
   end
 end
--- test/td_perms/acl_cache_test.exs
+++ test/td_perms/acl_cache_test.exs
@@ -3,35 +3,59 @@ defmodule TdPerms.AclCacheTest do
   alias TdPerms.AclCache
   doctest TdPerms.AclCache
 
-  test "put_user_roles returns Ok" do
-    user_roles = user_roles_fixture()
-    assert AclCache.put_user_roles(user_roles) == {:ok, "OK"}
+  test "set_acl_roles returns Ok" do
+    resource_id = 1
+    resource_type = "test_type"
+    roles = ["role1", "role2", "role3"]
+    assert AclCache.set_acl_roles(resource_id,resource_type,roles) == {:ok, length(roles)}
   end
 
-  test "get_user_roles" do
-    user_roles = user_roles_fixture()
-    AclCache.put_user_roles(user_roles)
-    user_roles_result = AclCache.get_user_roles(
-      user_roles.resource_type, user_roles.resource_id)
-    assert user_roles_result == user_roles.user_roles
+  test "get_acl_roles returns same value that was put" do
+    resource_id = 1
+    resource_type = "test_type"
+    roles = MapSet.new(["role1", "role2", "role3"])
+    AclCache.set_acl_roles(resource_id, resource_type, roles)
+    roles_result = AclCache.get_acl_roles(resource_id, resource_type)
+    assert MapSet.new(roles_result) == roles
   end
 
-  test "delete_user_roles deletes from cache" do
-    user_roles = user_roles_fixture()
-    AclCache.put_user_roles(user_roles)
-    AclCache.delete_user_roles(user_roles.resource_type, user_roles.resource_id)
-    key = AclCache.create_key(user_roles.resource_type, user_roles.resource_id)
+  test "delete_acl_roles deletes from cache" do
+    resource_id = 1
+    resource_type = "test_type"
+    roles = ["role1", "role2", "role3"]
+    AclCache.set_acl_roles(resource_id, resource_type, roles)
+    AclCache.delete_acl_roles(resource_id, resource_type)
+    key = AclCache.create_acl_roles_key(resource_id, resource_type)
     assert {:ok, 0} = Redix.command(:redix, ["EXISTS", "#{key}"])
   end
 
-  defp user_roles_fixture do
-    %{
-      resource_type: "type",
-      resource_id: 1,
-      user_roles: [%{
-        "role_name" => "role1", "users" => ["user1"]
-      }]
-    }
+  test "set_acl_role_users returns Ok" do
+    resource_id = 1
+    resource_type = "test_type"
+    role = "role1"
+    users = ["user1", "user2", "user3"]
+    result = AclCache.set_acl_role_users(resource_id, resource_type, role, users)
+    assert result == {:ok, length(users)}
   end
 
+  test "get_acl_role_users returns same value that was put" do
+    resource_id = 1
+    resource_type = "test_type"
+    role = "role1"
+    users = MapSet.new(["user1", "user2", "user3"])
+    AclCache.set_acl_role_users(resource_id, resource_type, role, users)
+    users_result = AclCache.get_acl_role_users(resource_id, resource_type, role)
+    assert MapSet.new(users_result) == users
+  end
+
+  test "delete_acl_role_users deletes from cache" do
+    resource_id = 1
+    resource_type = "test_type"
+    role = "role1"
+    users = ["user1", "user2", "user3"]
+    AclCache.set_acl_role_users(resource_id, resource_type, role, users)
+    AclCache.delete_acl_role_users(resource_id, resource_type, role)
+    key = AclCache.create_acl_role_users_key(resource_id, resource_type, role)
+    assert {:ok, 0} = Redix.command(:redix, ["EXISTS", "#{key}"])
+  end
 end
