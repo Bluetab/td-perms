e3a2c967fd1aa2f83b6759cfab50ed9bb23903f7
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-10-30 15:30:02 +0100

    feat: New function used to retrieve all the users existing in cache

--- lib/td_perms/user_cache.ex
+++ lib/td_perms/user_cache.ex
@@ -23,6 +23,45 @@ defmodule TdPerms.UserCache do
     Redix.command(:redix, ["DEL", key])
   end
 
+  def get_all_users do
+    cursor = 0
+    key = "user:*"
+    {next_cursor, list_users} = retrieve_list_from_enumerator(cursor, key)
+    loop_over_scan_iterations(key, list_users, next_cursor)
+  end
+
+  defp loop_over_scan_iterations(_key, acc_list_users, 0), do: acc_list_users
+
+  defp loop_over_scan_iterations(key, acc_list_users, cursor) do
+    {next_cursor, list_users} = retrieve_list_from_enumerator(cursor, key)
+    acc_list_users = acc_list_users ++ list_users
+    loop_over_scan_iterations(key, acc_list_users, next_cursor)
+  end
+
+  defp retrieve_list_from_enumerator(cursor, key) do
+    {:ok, [head | tail]} = scan_command(cursor, key)
+    list_users = get_users_from_list_keys(tail |> List.flatten())
+    {String.to_integer(head), list_users}
+  end
+
+  defp get_users_from_list_keys(list_user_keys) do
+    list_user_keys |> Enum.map(fn uk ->
+      user_id =
+        uk
+          |> String.split(":")
+          |> List.last()
+          |> String.to_integer()
+
+      user_id
+        |> get_user()
+        |> Map.merge(Map.new() |> Map.put(:id, user_id))
+    end)
+  end
+
+  defp scan_command(cursor, key) do
+    Redix.command(:redix, ["SCAN", cursor, "MATCH", key])
+  end
+
   defp create_key(id) do
     "user:#{id}"
   end
