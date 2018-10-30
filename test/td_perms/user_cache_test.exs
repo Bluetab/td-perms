e4daeb16b91def7ed17698dd80bdd54c7a90b729
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-10-30 15:29:13 +0100

    feat: Add new fucntion to retrieve a complete list of users

--- test/td_perms/user_cache_test.exs
+++ test/td_perms/user_cache_test.exs
@@ -1,7 +1,14 @@
 defmodule TdPerms.UserCacheTest do
+  @moduledoc false
   use ExUnit.Case
   alias TdPerms.UserCache
   doctest TdPerms.UserCache
+
+  @user_list [
+    %{id: 101, user_name: "User name 101", full_name: "fool name 101", email: "some.email101@blah.com"},
+    %{id: 102, user_name: "User name 102", full_name: "fool name 102", email: "some.email102@blah.com"},
+    %{id: 103, user_name: "User name 103", full_name: "fool name 103", email: "some.email103@blah.com"}
+  ]
 
   test "put_user returns OK" do
     user = user_fixture()
@@ -25,7 +32,24 @@ defmodule TdPerms.UserCacheTest do
     assert {:ok, 0} = Redix.command(:redix, ["EXISTS", "user:#{user.id}"])
   end
 
+  test "get_all_users returns a list with all the existing users" do
+    user_list_fixture()
+    result_list = UserCache.get_all_users()
+    assert @user_list
+      |> Enum.all?(fn x -> Enum.any?(result_list, &(&1 == x)) end)
+  end
+
   defp user_fixture do
     %{id: 42, user_name: "User name", full_name: "fool name", email: "some.email@blah.com"}
+  end
+
+  defp user_list_fixture do
+    delete_users(@user_list)
+    @user_list
+      |> Enum.map(&UserCache.put_user(&1))
+  end
+
+  defp delete_users(user_list) do
+    user_list |> Enum.map(&UserCache.delete_user(Map.get(&1, :id)))
   end
 end
