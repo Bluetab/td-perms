6baee53482aeedc3d5a57d7754dfe93ea04f49c7
netto <euclydes.young@bluetab.net>
2019-03-10 21:24:06 +0100

    feat: added list_templates_by_scope to df mock

--- test/td_perms/mocks/mock_df_cache_test.exs
+++ test/td_perms/mocks/mock_df_cache_test.exs
@@ -29,16 +29,6 @@ defmodule TdPerms.MockMockDynamicFormCacheTest do
     assert MockDynamicFormCache.get_template_by_name(template.name) == template
   end
 
-  test "get_template_by_name invalid key will return nil" do
-    assert MockDynamicFormCache.get_template_by_name("invalid:key") == nil
-  end
-
-  test "get_default_template will return the default template" do
-    template = df_fixture_default()
-    MockDynamicFormCache.put_template(template)
-    assert MockDynamicFormCache.get_default_template() == Map.drop(template, [:is_default])
-  end
-
   test "list_templates will return a list of objects" do
     template1 = df_fixture("t1")
     template2 = df_fixture("t2")
@@ -47,6 +37,15 @@ defmodule TdPerms.MockMockDynamicFormCacheTest do
     MockDynamicFormCache.put_template(template2)
     MockDynamicFormCache.put_template(template3)
     assert length(MockDynamicFormCache.list_templates()) >= 3
+  end
+
+  test "list_templates_by_scope will only return template from the requested scope" do
+    MockDynamicFormCache.clean_cache()
+    MockDynamicFormCache.put_template(df_scope_fixture("t1", "s1"))
+    MockDynamicFormCache.put_template(df_scope_fixture("t2", "s2"))
+    MockDynamicFormCache.put_template(df_scope_fixture("t3", "s1"))
+    MockDynamicFormCache.put_template(df_scope_fixture("t4", "s3"))
+    assert length(MockDynamicFormCache.list_templates_by_scope("s1")) == 2
   end
 
   test "delete_template deletes from cache" do
@@ -69,20 +68,32 @@ defmodule TdPerms.MockMockDynamicFormCacheTest do
   end
 
   defp df_fixture do
-    %{id: 0, name: "test", content: [%{"name" => "field", "type" => "string"}], label: "label"}
+    %{
+      id: 0,
+      name: "test",
+      content: [%{"name" => "field", "type" => "string"}],
+      label: "label",
+      scope: "scope"
+    }
   end
 
   defp df_fixture(name) do
-    %{id: 0, name: name, content: [%{"name" => "field", "type" => "string"}], label: "label"}
-  end
-
-  defp df_fixture_default do
     %{
       id: 0,
-      name: "test_default",
+      name: name,
       content: [%{"name" => "field", "type" => "string"}],
       label: "label",
-      is_default: true
+      scope: "scope"
+    }
+  end
+
+  defp df_scope_fixture(name, scope) do
+    %{
+      id: 0,
+      name: name,
+      content: [%{"name" => "field", "type" => "string"}],
+      label: "label",
+      scope: scope
     }
   end
 end
